d0e77b7c43b8d962ff394c284f29b76fa1e0ede5
HEEJI YUN <yhj8147e@gmail.com>
2022-11-30 20:33:12 +0900

    init: [윤희지] 팀 프로젝트 next-page flutter 환경 구축

---
+++ next-page-flutter/app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text("초기화"))
+    );
+  }
+}
