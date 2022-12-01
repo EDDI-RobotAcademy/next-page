3eea4e79a0eff969665457f69dd9d54bd7553b57
Hanchaeseon <mdmodina34@gmail.com>
2022-12-01 21:50:59 +0900

    feat.[한채선] 포인트 충전 ui 및 결제 모듈 연결 구현중  [NEXT-PAGE-WEB-UI-8]

---
+++ next-page-vue/frontend/src/views/Payment/PaymentSuccessView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-text>
+        <h5>포인트 충전이 완료되었습니다.</h5>
+        메인으로 돌아갑니다.
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "PaymentSuccessView",
+
+  mounted() {
+    setTimeout(()=> { this.$router.push("/") }, 3000) // 3초후 자동으로 메인으로 돌아감
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
