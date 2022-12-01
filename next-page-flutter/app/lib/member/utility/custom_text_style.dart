e98a316905a122b20dd8bc55a962e4c141850a2e
HEEJI YUN <yhj8147e@gmail.com>
2022-12-01 21:56:14 +0900

    feat: [윤희지] text style 양식 [NEXT-PAGE-APP-UI-1]

---
+++ next-page-flutter/app/lib/member/utility/custom_text_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+class CustomTextStyle {
+  static const TextStyle title = TextStyle(
+    fontSize: 40,
+    color: Color(0xff6699FF),
+    fontWeight: FontWeight.bold
+  );
+}
