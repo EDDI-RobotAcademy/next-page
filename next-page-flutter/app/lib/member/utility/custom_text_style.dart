c978d8c3ac3dcbe2cb82b5055dd984a94e726fdf
HEEJI YUN <yhj8147e@gmail.com>
2022-12-05 21:35:58 +0900

    feat: [윤희지] 로그인 페이지 UI 구현 완료 [NEXT-PAGE-APP-UI-1]

--- next-page-flutter/app/lib/member/utility/custom_text_style.dart
+++ next-page-flutter/app/lib/member/utility/custom_text_style.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/cupertino.dart';
-class CustomTextStyle {
-  static const TextStyle title = TextStyle(
+import 'package:flutter/material.dart';
+
+const TextStyle largeTextStyleMain = TextStyle(
     fontSize: 40,
     color: Color(0xff6699FF),
     fontWeight: FontWeight.bold
   );
-}
+
+const TextStyle smallTextStyleWhite = TextStyle(
+  fontSize: 15,
+  color: Colors.white
+);
+
+const TextStyle smallTextStyleMain = TextStyle(
+    fontSize: 15,
+    color: Color(0xff6699FF)
+);
+
+
+
