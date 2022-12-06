1e91b04d61bf6c1423562de7175b2584a55d4b8f
jssnk <ssnssnl@kakao.com>
2022-12-06 23:08:51 +0900

    feat:[김장순] 소설 상세보기 페이지 UI 테스트를 위한 임시 model 추가 [NEXT-PAGE-APP-UI]

---
+++ next-page-flutter/app/lib/model/tmp_novel_episode.dart
@@ -0,0 +1,82 @@
+class TmpEpisodeModel {
+  int episode;
+  String title;
+  String regDate;
+
+
+  TmpEpisodeModel(
+      {
+        this.episode = 0,
+        this.title = '',
+        this.regDate = '',
+      });
+
+  static List<TmpEpisodeModel> episodeList = <TmpEpisodeModel>[
+    TmpEpisodeModel(
+        episode: 1,
+        title: '근육조선',
+        regDate: '2022.12.01',
+    ),
+    TmpEpisodeModel(
+        episode: 2,
+        title: '근육조선',
+        regDate: '2022.12.01',
+    ),
+    TmpEpisodeModel(
+        episode: 3,
+        title: '근육조선',
+        regDate: '2022.12.02',
+    ),
+    TmpEpisodeModel(
+        episode: 4,
+        title: '근육조선',
+        regDate: '2022.12.02',
+    ),
+    TmpEpisodeModel(
+        episode: 5,
+        title: '근육조선',
+        regDate: '2022.12.03',
+    ),
+    TmpEpisodeModel(
+        episode: 6,
+        title: '근육조선',
+        regDate: '2022.12.03',
+    ),
+    TmpEpisodeModel(
+        episode: 7,
+        title: '근육조선',
+        regDate: '2022.12.04',
+    ),
+    TmpEpisodeModel(
+        episode: 8,
+        title: '근육조선',
+        regDate: '2022.12.04',
+    ),
+    TmpEpisodeModel(
+        episode: 9,
+        title: '근육조선',
+        regDate: '2022.12.05',
+    ),
+    TmpEpisodeModel(
+        episode: 10,
+        title: '근육조선',
+        regDate: '2022.12.05',
+    ),
+    TmpEpisodeModel(
+        episode: 11,
+        title: '근육조선',
+        regDate: '2022.12.06',
+    ),
+    TmpEpisodeModel(
+        episode: 12,
+        title: '근육조선',
+        regDate: '2022.12.06',
+    ),
+    TmpEpisodeModel(
+        episode: 13,
+        title: '근육조선',
+        regDate: '2022.12.06',
+    ),
+
+  ];
+}
