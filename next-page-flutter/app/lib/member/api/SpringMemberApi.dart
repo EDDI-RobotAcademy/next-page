608d156e1c2b9723746462f0d8ddb1ec0464edc2
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2022-12-15 19:45:04 +0900

    feat: [윤희지] 회원 가입 관련 API 구현 완료 [NEXT-PAGE-APP-UI-8]

--- next-page-flutter/app/lib/member/api/SpringMemberApi.dart
+++ next-page-flutter/app/lib/member/api/SpringMemberApi.dart
@@ -2,11 +2,70 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+import '../../http_uri.dart';
 import 'requests.dart';
 import 'responses.dart';
 
 class SpringMemberApi {
-  String httpUri = '192.168.1.104:7777';
+
+  Future<bool?> emailCheck ( String email ) async {
+    var data = { 'email': email };
+    var body = json.encode(data);
+
+    var response = await http.post(
+      Uri.http(httpUri, '/member/check-email/$email'),
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint("통신 확인");
+      return json.decode(response.body);
+    } else {
+      throw Exception("통신 실패");
+    }
+  }
+
+  Future<bool?> nicknameCheck (String nickName) async {
+    var data = { 'nickName': nickName };
+    var body = json.encode(data);
+
+    var response = await http.post(
+      Uri.http(httpUri, '/member/check-nickname/$nickName'),
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint("통신 확인");
+      return json.decode(response.body);
+    } else {
+      throw Exception("통신 실패");
+    }
+  }
+
+  Future<bool?> signUp (SignUpRequest request) async {
+    var data = { 'email': request.email, 'password': request.password, 'nickName': request.nickName };
+    var body = json.encode(data);
+
+    debugPrint(request.email);
+    debugPrint(request.password);
+    debugPrint(request.nickName);
+    debugPrint(body);
+
+    var response = await http.post(
+      Uri.http(httpUri, '/member/sign-up'),
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint("통신 확인");
+      return json.decode(response.body);
+    } else {
+      throw Exception("통신 실패");
+    }
+  }
 
   Future<SignInResponse> signIn (SignInRequest request) async {
     var data = { 'email': request.email, 'password': request.password };
@@ -17,18 +76,18 @@ class SpringMemberApi {
     debugPrint(body);
 
     var response = await http.post(
-      Uri.http(httpUri, '실제 요청 주소'),
+      Uri.http(httpUri, '/member/sign-in'),
       headers: {"Content-Type": "application/json"},
       body: body,
     );
 
     if (response.statusCode == 200) {
       debugPrint("통신 확인");
-      debugPrint(response.body);
+      debugPrint('userToken: '+ response.body);
 
       return SignInResponse(true, response.body);
     } else {
-      debugPrint("로그인 실패");
+      debugPrint("통신 실패");
       return SignInResponse(false, '로그인 실패!');
     }
   }
