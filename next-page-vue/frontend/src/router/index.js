d447641ff1c39272fcd98b38917c8d2c0e0f9849
k724k <109579337+k724k@users.noreply.github.com>
2022-12-20 16:02:23 +0900

    feat: [이연경] 회원가입 UI [NEXT-PAGE-WEB-UI-2]

--- next-page-vue/frontend/src/router/index.js
+++ next-page-vue/frontend/src/router/index.js
@@ -3,6 +3,18 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PointChargeView from "@/views/Payment/PointChargeView";
 import PaymentSuccessView from "@/views/Payment/PaymentSuccessView";
+import SignUpView from "@/views/account/SignUpView";
+import SignInView from "@/views/account/SignInView";
+import MyPageView from "@/views/myPage/MyPageView";
+import MyPageModify from "@/component/myPage/MyPageModify";
+import MemberJoinForm from "@/component/account/MemberJoinForm";
+import MyPageTest from "@/component/myPage/MyPageTest";
+import MyPageOut from "@/component/myPage/MyPageOut";
+import MyPageNotice from "@/component/myPage/MyPageNotice";
+import MyPageQnA from "@/component/myPage/MyPageQnA";
+
+
+
 
 Vue.use(VueRouter)
 
@@ -31,7 +43,72 @@ const routes = [
     props: {
       paymentData: true
     }*/
-  }
+  },
+
+
+  {
+    path: '/SignUp-View',
+    name: 'SignUp-View',
+    component: SignUpView
+  },
+
+  {
+    path: '/SignIn-View',
+    name: 'SignIn-View',
+    component: SignInView
+  },
+
+  {
+    path: '/MyPage-View',
+    name: 'MyPage-View',
+    component: MyPageView
+  },
+
+  {
+    path: '/MyPage-Modify',
+    name: 'MyPage-Modify',
+    component: MyPageModify
+  },
+  {
+    path: '/myPage',
+    name: 'MyPageView',
+    component: MyPageView
+  },
+
+  {
+    path: '/Member-Join-Form',
+    name: 'Member-Join-Form',
+    component: MemberJoinForm
+  },
+
+  {
+    path: '/MyPage-Out',
+    name: 'MyPage-Out',
+    component: MyPageOut
+  },
+
+  {
+    path: '/MyPage-Notice',
+    name: 'MyPage-Notice',
+    component: MyPageNotice
+  },
+
+  {
+    path: '/MyPage-QnA',
+    name: 'MyPage-QnA',
+    component: MyPageQnA
+  },
+
+
+
+
+  {
+    path: '/test', //테스트 용
+    name: 'test',
+    component: MyPageTest
+  },
+
+
 
 
 ]
