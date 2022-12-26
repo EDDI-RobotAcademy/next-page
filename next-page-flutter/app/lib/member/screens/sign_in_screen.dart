b17ff3628fb03096ed26c224fc6c01e0acc11753
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2022-12-26 19:38:20 +0900

    update: [윤희지] 로그인 페이지 앱바 추가 [NEXT-PAGE-APP-UI-1]

--- next-page-flutter/app/lib/member/screens/sign_in_screen.dart
+++ next-page-flutter/app/lib/member/screens/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../custom_transparent_appbar.dart';
 import '../widgets/forms/sign_in_form.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -13,6 +14,7 @@ class SignInScreen extends StatelessWidget {
           FocusScope.of(context).unfocus();
         },
         child: Scaffold(
+          appBar: customTransparentAppbar(),
             body: SignInForm())
     );
   }
