d8bf2f25e44ec402579e8b218f87ea5c49bc32e4
k724k <109579337+k724k@users.noreply.github.com>
2022-12-27 16:20:43 +0900

    feat :[이연경] QnA 게시글 작성 UI[NEXT-PAGE-WEB-UI-18]

---
+++ next-page-vue/frontend/src/views/mypage/qna/QnARegisterView.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+
+
+  <div align="center">
+
+    <QnARegister @submit="onSubmit"/>
+  </div>
+
+
+</div>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+import QnARegister from "@/component/mypage/qna/QnARegister";
+
+
+export default {
+  name: "QnARegisterView",
+  components: {QnARegister},
+  methods: {
+    ...mapActions([
+      'requestCreateBoardContentsToSpring'
+    ]),
+    async onSubmit (payload) {
+      await this.requestCreateBoardContentsToSpring(payload)
+      await this.$router.push({
+        name: 'QnA-List-View'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
