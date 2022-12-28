3a4c9f068cb8505a29afb5619ad6ad053b37bc3a
Hanchaeseon <mdmodina34@gmail.com>
2022-12-28 20:06:43 +0900

    feat.[한채선] 소설 리스트 관리 페이지 UI [NEXT-PAGE-WEB-UI-10]

---
+++ next-page-vue/frontend/src/component/Upload/ContentSimpleUi.vue
@@ -0,0 +1,32 @@
+<template>
+  <router-link :to=" { name: 'InformationDetail',
+                       params: { novelInfoNo : novelInfo.id }} ">
+    <v-card max-width="200px" class="pa-2">
+      <v-img height="250px" :src="require(`@/assets/coverImages/${novelInfo.coverImage.reName}`)"/>
+      <v-card-text class="justify-center">
+        {{ novelInfo.title}}
+      </v-card-text>
+      <v-card-text>
+        <v-icon>
+          mdi-account-edit
+        </v-icon>
+        {{ novelInfo.author }}
+      </v-card-text>
+    </v-card>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "ContentSimpleUi",
+  props: {
+    novelInfo : {
+      type: Object,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
