19e81a3dbf7492f90662f2d425e310dfbd110132
k724k <109579337+k724k@users.noreply.github.com>
2022-12-29 20:45:02 +0900

    update: [이연경] 로그인 Test 로직 코드수정[NEXT-PAGE-PROCESS-2]

--- next-page-backend/demo/src/test/java/kr/eddi/demo/member/MemberTest.java
+++ next-page-backend/demo/src/test/java/kr/eddi/demo/member/MemberTest.java
@@ -1,11 +1,15 @@
 package kr.eddi.demo.member;
 
+import kr.eddi.demo.member.entity.member.NextPageMember;
 import kr.eddi.demo.member.entity.service.MemberServiceImpl;
 import kr.eddi.demo.member.entity.service.member.request.MemberSignInRequest;
 import kr.eddi.demo.member.entity.service.member.request.MemberSignUpRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.Map;
 
 @SpringBootTest
 
@@ -16,34 +20,44 @@ public class MemberTest {
 
     @Test
     void checkEmail() { // 이메일 Test
-        boolean Success = service.emailValidation("test1@test.com");
+        boolean Success = service.emailValidation("test0@test.com");
         System.out.println("Success: " + Success);
     }
 
 
     @Test
-    public void nickNameCheck(){ //닉네임 Test
-        boolean Success = service.nickNameValidation("test");
+    public void nickNameCheck() { //닉네임 Test
+        boolean Success = service.nickNameValidation("test0");
         System.out.println("Success: " + Success);
     }
 
 
     @Test
     public void SignUpTest() { // 회원가입 Test
-        MemberSignUpRequest request = new MemberSignUpRequest("test1@test.com","00000","test");
+        MemberSignUpRequest request = new MemberSignUpRequest("test0@test.com", "00000000", "test0");
         boolean Success = service.signUp(request);
 
-        System.out.println(Success);
+        System.out.println("Success: " + Success);
     }
 
 
     @Test
-    public void SignInTest() { // 로그인 Test
-        MemberSignInRequest request = new MemberSignInRequest("test1@test.com", "00000");
-        String token = service.signIn(request);
+    @Transactional
 
+    public void SignInTest() {  // 로그인 Test
+        MemberSignInRequest request = new MemberSignInRequest("test0@test.com", "00000000");
+
+        Map<String, String> memberInfo = service.signIn(request);
+        String token = memberInfo.get("userToken");
         System.out.println(token);
+
     }
+
+
+
+
+
+
 
 
 
