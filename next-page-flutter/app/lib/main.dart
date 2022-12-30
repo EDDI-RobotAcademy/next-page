be11869dde5ed2b15f0ad9b5a39dfea40543c2ee
KimJangsoon <ssnssnl@kakao.com>
2022-12-30 15:22:47 +0900

    chore:[김장순] main페이지 import구문 수정 [NEXT-PAGE-APP-UI]

--- next-page-flutter/app/lib/main.dart
+++ next-page-flutter/app/lib/main.dart
@@ -1,14 +1,12 @@
-
-import 'package:app/member/screens/sign_in_screen.dart';
-import 'package:app/member/screens/sign_up_screen.dart';
-
-import 'package:app/member/utility/user_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-import 'custom_bottom_appbar.dart';
+
+import 'widgets/custom_bottom_appbar.dart';
 import 'member/screens/sign_in_screen.dart';
+import 'member/screens/sign_up_screen.dart';
+import 'member/utility/user_data_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,22 +18,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (BuildContext context) => UserDataProvider()),
-      ],
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-              appBarTheme: const AppBarTheme(
-                  systemOverlayStyle: SystemUiOverlayStyle.dark
-              )
-          ),
-          home: CustomBottomAppbar(routeIndex: 0,),
-          routes: {
-            "/sign-in": (context) => const SignInScreen(fromWhere: 0, novel: "none"),
-            "/sign-up": (context) => const SignUpScreen(),
-          }
-      )
+        providers: [
+          ChangeNotifierProvider(create: (BuildContext context) => UserDataProvider()),
+        ],
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                appBarTheme: const AppBarTheme(
+                    systemOverlayStyle: SystemUiOverlayStyle.dark
+                )
+            ),
+            home: CustomBottomAppbar(routeIndex: 0,),
+            routes: {
+              "/sign-in": (context) => const SignInScreen(fromWhere: 0, novel: "none",),
+              "/sign-up": (context) => const SignUpScreen(),
+            }
+        )
     );
   }
 }
