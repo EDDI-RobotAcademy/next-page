f0985e91d6cea055cdd1792e6af609ae8e946455
k724k <109579337+k724k@users.noreply.github.com>
2023-01-03 23:03:49 +0900

    style: [이연경] 포인트 충전 페이지 + headline + Footer 추가[NEXT-PAGE-WEB-UI-8]

--- next-page-vue/frontend/src/views/Payment/PointChargeView.vue
+++ next-page-vue/frontend/src/views/Payment/PointChargeView.vue
@@ -1,14 +1,27 @@
 <template>
-  <v-container>
+  <div>
+
+
+    <headline-menu-form/>
+
+    <br><br><br><br>
     <PointChargeForm/>
-  </v-container>
+
+
+
+    <br><br><br><br><br><br><br>
+  <footer-menu-form/>
+  </div>
 </template>
 
 <script>
 import PointChargeForm from "@/component/PointCharge/PointChargeForm";
+import HeadlineMenuForm from "@/component/header/HeadlineMenuForm";
+import FooterMenuForm from "@/component/footer/FooterMenuForm";
+
 export default {
   name: "PointChargeView",
-  components: {PointChargeForm}
+  components: {FooterMenuForm, HeadlineMenuForm,  PointChargeForm}
 }
 </script>
 
