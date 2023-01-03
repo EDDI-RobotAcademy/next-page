eb26b423cb4d399455f8d064e34861f70cf48d9e
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2023-01-03 16:58:11 +0900

    add: [윤희지] 소설 에피소드 구매 test 누락 추가 [NEXT-PAGE-PROCESS-27]

---
+++ next-page-backend/demo/src/test/java/kr/eddi/demo/EpisodePaymentTest.java
@@ -0,0 +1,20 @@
+package kr.eddi.demo;
+
+import kr.eddi.demo.episode_payment.requset.BuyEpisodeRequest;
+import kr.eddi.demo.episode_payment.service.EpisodePaymentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EpisodePaymentTest {
+
+    @Autowired
+    EpisodePaymentService episodePaymentService;
+    // 테스트할 때 꼭! 소설등록, 에피소드 등록 먼저 해야합니다!!
+    @Test
+    void buyEpisodeTest() {
+        BuyEpisodeRequest req = new BuyEpisodeRequest(1L, 8L);
+        episodePaymentService.buyEpisode(req);
+    }
+}
