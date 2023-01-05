4624011056bf5add35c199cef2dbbf496d1fd8a1
k724k <109579337+k724k@users.noreply.github.com>
2023-01-05 17:17:58 +0900

    fix: [이연경] MemberNicknameModifyRequest 불필요한 코드삭제[NEXT-PAGE-PROCESS-6]

--- next-page-backend/demo/src/main/java/kr/eddi/demo/member/entity/service/member/request/MemberNicknameModifyRequest.java
+++ next-page-backend/demo/src/main/java/kr/eddi/demo/member/entity/service/member/request/MemberNicknameModifyRequest.java
@@ -1,7 +1,6 @@
 package kr.eddi.demo.member.entity.service.member.request;
 
 
-import kr.eddi.demo.member.entity.member.NextPageMember;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -19,11 +18,5 @@ public class MemberNicknameModifyRequest {
     private String email;
 
     private String reNickName;
-
-    public NextPageMember toMember(){
-        return new NextPageMember(email,reNickName);
-
-    }
-
 
 }
