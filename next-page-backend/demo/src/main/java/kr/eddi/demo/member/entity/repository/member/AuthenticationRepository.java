a6de6423d8526e5d9902746c4bbf4a5a3bf97967
k724k <109579337+k724k@users.noreply.github.com>
2023-01-05 16:55:26 +0900

    chore: [이연경] 불필요한 코드 수정[NEXT-PAGE-PROCESS]

--- next-page-backend/demo/src/main/java/kr/eddi/demo/member/entity/repository/member/AuthenticationRepository.java
+++ next-page-backend/demo/src/main/java/kr/eddi/demo/member/entity/repository/member/AuthenticationRepository.java
@@ -2,9 +2,13 @@ package kr.eddi.demo.member.entity.repository.member;
 
 import kr.eddi.demo.member.entity.member.Authentication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface AuthenticationRepository extends JpaRepository<Authentication, Long> {
 
-
+    @Query("select a from Authentication a where a.member.id = :member_id")
+    Optional<Authentication> findByMemberId(Long member_id);
 
 }
