922452546e60bf722d0f5f176997a1c9c2ca91c2
KimJangsoon <ssnssnl@kakao.com>
2023-01-11 06:17:32 +0900

    feat: [김장순] 구매한 에피소드의 리스트를 조회하는 시스템 [NEXT-PAGE-PROCESS-32]

--- next-page-backend/demo/src/main/java/kr/eddi/demo/episode_payment/repository/EpisodePaymentRepository.java
+++ next-page-backend/demo/src/main/java/kr/eddi/demo/episode_payment/repository/EpisodePaymentRepository.java
@@ -2,6 +2,12 @@ package kr.eddi.demo.episode_payment.repository;
 
 import kr.eddi.demo.episode_payment.entity.EpisodePayment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EpisodePaymentRepository extends JpaRepository<EpisodePayment, Long>{
+
+    @Query("select ep from EpisodePayment ep join fetch ep.member m join fetch ep.novel n where m.id = :memberId and n.id = :novelId")
+    List<EpisodePayment> findEpisodeListByMemberAndNovel(Long memberId, Long novelId);
 }
--- next-page-backend/demo/src/main/java/kr/eddi/demo/episode_payment/service/EpisodePaymentService.java
+++ next-page-backend/demo/src/main/java/kr/eddi/demo/episode_payment/service/EpisodePaymentService.java
@@ -1,7 +1,12 @@
 package kr.eddi.demo.episode_payment.service;
 
 import kr.eddi.demo.episode_payment.requset.BuyEpisodeRequest;
+import kr.eddi.demo.episode_payment.requset.GetEpisodeListRequest;
+
+import java.util.List;
 
 public interface EpisodePaymentService {
     Boolean buyEpisode(BuyEpisodeRequest buyRequest);
+
+    List getPurchasedEpisodeList(GetEpisodeListRequest request);
 }
