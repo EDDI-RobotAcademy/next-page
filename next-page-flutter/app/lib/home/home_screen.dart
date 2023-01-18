a6564610867132439aa6120901bf136ab6451d70
KimJangsoon <ssnssnl@kakao.com>
2023-01-19 01:09:43 +0900

    feat:[김장순] my페이지가 아닌 곳에서 포인트 충전페이지로 진입 가능한 coin이미지가 있는 appbar [NEXT-PAGE-APP-UI-73]

--- next-page-flutter/app/lib/home/home_screen.dart
+++ next-page-flutter/app/lib/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import '../widgets/custom_title_appbar.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../app_theme.dart';
+import '../member/screens/sign_in_screen.dart';
+import '../point/screens/point_charge_screen.dart';
 import '../widgets/link_banner_box.dart';
 import 'widgets/custom_card_list.dart';
 import 'widgets/custom_carousel.dart';
@@ -15,12 +19,65 @@ class _HomeScreenState extends State<HomeScreen> {
 
   int _newNovelList = 0;
   int _liveHotList = 1;
+  bool _loginState = false;
+
+  @override
+  void initState() {
+    _asyncMethod();
+    super.initState();
+  }
+
+  void _asyncMethod() async {
+    var prefs = await SharedPreferences.getInstance();
+    String? userToken = prefs.getString('userToken');
+
+    if (userToken != null) {
+      setState(() {
+        _loginState = true;
+      });
+    } else {
+      setState(() {
+        _loginState = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: customTitleAppbar(context, 'NEXT PAGE', 0),
+      appBar: AppBar(
+        title: const Text(
+          '소설',
+          style: TextStyle(
+              color: AppTheme.pointColor, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Row(
+            children: [
+              InkWell(
+                onTap: () {
+                  _loginState?
+                  Get.to(() => const PointChargeScreen(
+                    fromWhere: 1,
+                  ))
+                      :Get.to(() => const SignInScreen(fromWhere: 0, novel: null, routeIndex: 0));
+                },
+                child: Container(
+                    width: MediaQuery.of(context).size.width * 0.07,
+                    child: Image.asset(
+                        'assets/images/coin_image_asset.png')),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.03,
+              )
+            ],
+          )
+
+        ],
+        elevation: 0.5,
+        backgroundColor: Colors.white,
+      ),
       body: Container(
         width: double.infinity,
         color: Colors.white,
@@ -35,7 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                 height: _size.height * 0.04,
               ),
-              LinkBannerBox(),
+              const LinkBannerBox(),
               SizedBox(
                 height: _size.height * 0.04,
               ),
