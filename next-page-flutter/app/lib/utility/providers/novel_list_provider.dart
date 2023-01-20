6a3243a04fdd88b6ecc9de8eb2b699c40188ad14
KimJangsoon <ssnssnl@kakao.com>
2023-01-20 16:10:11 +0900

    feat:[김장순] 카테고리별 소설 리스트를 분류해 정렬방법에 따라 리스트를 제공해주는 novelListProvider [NEXT-PAGE-APP-UI-79]

---
+++ next-page-flutter/app/lib/utility/providers/novel_list_provider.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import '../../novel/api/novel_responses.dart';
+import '../../novel/api/spring_novel_api.dart';
+
+class NovelListProvider extends ChangeNotifier {
+  List<NovelListResponse>? _fantasyNovelList;
+  List<NovelListResponse>? _chivalryNovelList;
+  List<NovelListResponse>? _romanceNovelList;
+  List<NovelListResponse>? _modernFantasyNovelList;
+  List<NovelListResponse>? _blNovelList;
+
+
+  List<NovelListResponse>? get fantasyNovelList => _fantasyNovelList;
+
+  List<NovelListResponse>? get chivalryNovelList => _chivalryNovelList;
+
+  List<NovelListResponse>? get romanceNovelList => _romanceNovelList;
+
+  List<NovelListResponse>? get modernFantasyNovelList =>
+      _modernFantasyNovelList;
+
+  List<NovelListResponse>? get blNovelList => _blNovelList;
+
+  void getNovelList(String categoryName) {
+    SpringNovelApi()
+        .getNovelListByCategory(categoryName)
+        .
+    then((value) {
+      if (categoryName == '판타지') {
+        _fantasyNovelList = value;
+        _fantasyNovelList!.sort((a,b) => b.createdDate.compareTo(a.createdDate));
+      } else if (categoryName == '무협') {
+        _chivalryNovelList = value;
+        _chivalryNovelList!.sort((a,b) => b.createdDate.compareTo(a.createdDate));
+      }
+      else if (categoryName == '로맨스') {
+        _romanceNovelList = value;
+        _romanceNovelList!.sort((a,b) => b.createdDate.compareTo(a.createdDate));
+
+      }
+      else if (categoryName == '현판') {
+        _modernFantasyNovelList = value;
+        _modernFantasyNovelList!.sort((a,b) => b.createdDate.compareTo(a.createdDate));
+
+      }
+      else if (categoryName == 'BL') {
+        _blNovelList = value;
+        _blNovelList!.sort((a,b) => b.createdDate.compareTo(a.createdDate));
+
+      }
+      notifyListeners();
+    }
+    );
+  }
+}
