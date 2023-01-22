5bac29cfe80639f8e94a1a7ffa62a469d4260bac
KimJangsoon <ssnssnl@kakao.com>
2023-01-23 00:38:17 +0900

    refactor:[김장순] 홈스크린에 소설리스트를 보여주는 슬라이드 카드 리스트 UI [NEXT-PAGE-APP-UI-31]

--- next-page-flutter/app/lib/home/widgets/custom_hot_card_list.dart
+++ next-page-flutter/app/lib/home/widgets/custom_hot_card_list.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
+import '../../novel/api/spring_novel_api.dart';
 import 'custom_vertical_card.dart';
 
-
 class CustomHotCardList extends StatefulWidget {
-  final List<dynamic> hotList;
-
-  const CustomHotCardList({Key? key, required this.hotList}) : super(key: key);
+  const CustomHotCardList({Key? key}) : super(key: key);
 
   @override
   State<CustomHotCardList> createState() => _CustomCardListState();
 }
 
 class _CustomCardListState extends State<CustomHotCardList> {
-  List<dynamic>? _hotList;
-
+  List<dynamic>? _hotNovelList;
+  var _future;
 
   @override
   void initState() {
-    setState(() {
-      _hotList = widget.hotList;
-      _hotList!.sort((a,b) => b.viewCount.compareTo(a.viewCount));
-    });
-    super.initState();
+    _future = _getHotNovelList();
   }
 
-Widget buildCardList(BuildContext context){
+  _getHotNovelList() async {
+    _hotNovelList = await SpringNovelApi().getShortNovelList(15);
+  }
+
+  Widget buildCardList(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Padding(
       padding: EdgeInsets.fromLTRB(size.width * 0.03, 0, size.width * 0.03, 0),
@@ -36,30 +34,47 @@ Widget buildCardList(BuildContext context){
             style: TextStyle(
                 fontSize: size.width * 0.05, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: size.height * 0.01,),
+          SizedBox(
+            height: size.height * 0.01,
+          ),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: SizedBox(
               height: size.height * 0.28,
               child: ListView.builder(
-                  itemCount: (_hotList!.length > 15)
-                      ? 15
-                      : _hotList!.length,
+                  itemCount: (_hotNovelList!.length > 15)
+                      ? 5
+                      : _hotNovelList!.length,
                   scrollDirection: Axis.horizontal,
                   shrinkWrap: true,
                   itemBuilder: (BuildContext context, int index) {
-                    return CustomVerticalCard(
-                        novel: _hotList![index]);
+                    return CustomVerticalCard(novel: _hotNovelList![index]);
                   }),
             ),
           ),
         ],
       ),
     );
-}
+  }
 
   @override
   Widget build(BuildContext context) {
-    return buildCardList(context);
+    return FutureBuilder(
+        future: _future,
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            } else {
+              return buildCardList(context);
+            }
+          } else {
+            return const Text("망");
+          }
+        }));
   }
 }
--- next-page-flutter/app/lib/main.dart
+++ next-page-flutter/app/lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
-
 import 'member/screens/member_join_screen.dart';
+import 'novel/api/novel_responses.dart';
+import 'novel/api/spring_novel_api.dart';
 import 'utility/providers/category_provider.dart';
 import 'utility/providers/comment_provider.dart';
 import 'utility/providers/episode_provider.dart';
 import 'utility/providers/notice_provider.dart';
+import 'utility/providers/novel_list_provider.dart';
 import 'utility/providers/qna_provider.dart';
 import 'widgets/custom_bottom_appbar.dart';
 import 'member/screens/sign_in_screen.dart';
@@ -24,27 +26,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (BuildContext context) => CategoryProvider()),
-          ChangeNotifierProvider(create: (BuildContext context) => QnaProvider()),
-          ChangeNotifierProvider(create: (BuildContext context) => CommentProvider()),
-          ChangeNotifierProvider(create: (BuildContext context) => EpisodeProvider()),
-          ChangeNotifierProvider(create: (BuildContext context) => NoticeProvider()),
-          ChangeNotifierProvider(create: (BuildContext context) => NovelListProvider())
+          ChangeNotifierProvider(
+              create: (BuildContext context) => CategoryProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => QnaProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => CommentProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => EpisodeProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => NoticeProvider()),
+          ChangeNotifierProvider(
+              create: (BuildContext context) => NovelListProvider()),
+          FutureProvider<List<NovelListResponse>>(
+              initialData: [],
+              create: (_) => SpringNovelApi().getAllNovelList()),
         ],
         child: GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-                appBarTheme: const AppBarTheme(
-                    systemOverlayStyle: SystemUiOverlayStyle.dark
-                )
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              appBarTheme: const AppBarTheme(
+                  systemOverlayStyle: SystemUiOverlayStyle.dark)),
+          home: CustomBottomAppbar(
+            routeIndex: 0,
+          ),
+          routes: {
+            "/sign-in": (context) => const SignInScreen(
+              fromWhere: 0,
+              novel: "none",
+              routeIndex: 99,
             ),
-            home: CustomBottomAppbar(routeIndex: 0,),
-            routes: {
-              "/sign-in": (context) => const SignInScreen(fromWhere: 0, novel: "none", routeIndex: 99,),
-              "/sign-up": (context) => const SignUpScreen(),
-              "/member-join" : (context) => const MemberJoinScreen(),
-            }
-        )
-    );
+            "/sign-up": (context) => const SignUpScreen(),
+            "/member-join": (context) => const MemberJoinScreen(),
+          },
+        ));
   }
 }
